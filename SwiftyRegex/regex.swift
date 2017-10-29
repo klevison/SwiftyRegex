9a19eb78ca1a68ad99028ad06e14235bbd966c15
Klevison Matias <klevison@gmail.com>
2017-10-29 14:08:28 -0300

    swift 3

--- SwiftyRegex/regex.swift
+++ SwiftyRegex/regex.swift
@@ -9,7 +9,7 @@
 import pcre
 
 /// internal extension to convert a string to an integer array
-extension SequenceType where Generator.Element == CChar {
+extension Sequence where Element == CChar {
     static func fromString(string: String) -> [CChar] {
         var temp = [CChar]()
         temp.reserveCapacity(string.utf8.count)
@@ -23,14 +23,14 @@ extension SequenceType where Generator.Element == CChar {
 
 /// Regular expression matcher based on pcre
 public class RegEx {
-    private let compiled: COpaquePointer
+    private let compiled: OpaquePointer
 
     public let pattern: String
     public let namedCaptureGroups:[String:Int]
     public let numCaptureGroups:Int
 
     /// Errors that may be thrown by initializer
-    public enum Error: ErrorType {
+    public enum RegExError: Error {
         /// Invalid pattern, contains error offset and error message from pcre engine
         case InvalidPattern(errorOffset: Int, errorMessage: String)
     }
@@ -42,16 +42,16 @@ public class RegEx {
     public init(pattern: String) throws {
         self.pattern = pattern
 
-        var tmp = [CChar].fromString(pattern)
+        var tmp = [CChar].fromString(string: pattern)
 
         var error = UnsafePointer<CChar>(bitPattern: 0)
         var errorOffset:Int32 = 0
         compiled = pcre_compile(&tmp, 0, &error, &errorOffset, nil)
         if compiled == nil {
-            let errorMessage = String.fromCString(error)!
+            let errorMessage = String(cString:error!)
             self.namedCaptureGroups = [String:Int]()
             self.numCaptureGroups = 0
-            throw RegEx.Error.InvalidPattern(errorOffset: Int(errorOffset), errorMessage: errorMessage)
+            throw RegEx.RegExError.InvalidPattern(errorOffset: Int(errorOffset), errorMessage: errorMessage)
         }
 
         // number of capture groups
@@ -63,7 +63,7 @@ public class RegEx {
         var named = [String:Int]()
         pcre_fullinfo(self.compiled, nil, Int32(PCRE_INFO_NAMECOUNT), &patternCount)
         if patternCount > 0 {
-            var name_table = UnsafeMutablePointer<UInt8>()
+            var name_table = UnsafeMutablePointer<UInt8>.allocate(capacity: 64)
             var name_entry_size: Int = 0
 
             pcre_fullinfo(self.compiled, nil, Int32(PCRE_INFO_NAMETABLE), &name_table)
@@ -71,19 +71,24 @@ public class RegEx {
 
             for i: Int in 0..<patternCount {
                 let offset = name_entry_size * i
-                let num = (Int(name_table.advancedBy(offset).memory) << 8) + Int(name_table.advancedBy(offset + 1).memory)
+                let num = (Int(name_table.advanced(by: offset).pointee) << 8) + Int(name_table.advanced(by: offset + 1).pointee)
 
                 // pattern name
-                var patternName = [UInt8](count: name_entry_size + 1, repeatedValue: 0)
+                var patternName = [UInt8](repeating: 0, count: name_entry_size + 1)
                 for idx in (name_entry_size * i + 2)..<(name_entry_size * (i + 1)) {
                     patternName[idx - (name_entry_size * i + 2)] = name_table[idx]
                 }
 
-                guard let patternNameString = String.fromCString(UnsafePointer<CChar>(patternName)) else {
-                    continue
+//                let str1 = String(cString: UnsafePointer<CChar>(patternName))
+//                let str2: String? =
+//
+//                guard let patternNameString: String? = String(cString: patternName) else {
+//                    continue
+//                }
+                if (String(cString: patternName).characters.count > 0){
+                    named[String(cString: patternName)] = num
                 }
-
-                named[patternNameString] = num
+                
             }
         }
         self.namedCaptureGroups = named
@@ -98,9 +103,9 @@ public class RegEx {
     /// - parameter string: the string to match
     /// - returns: tuple with matches, named parameters are returned as numbered matches too
     public func match(string: String) -> (numberedParams:[String], namedParams:[String:String]) {
-	    var outVector = [Int32](count: 30, repeatedValue: 0)
+        var outVector = [Int32](repeating: 0, count: 30)
 
-        let subject = [CChar].fromString(string)
+        let subject = [CChar].fromString(string: string)
 
         // pcre_exec does not like zero terminated strings (???)
         let resultCount = pcre_exec(self.compiled, nil, subject, Int32(subject.count-1), 0, 0, &outVector, 30)
@@ -126,12 +131,13 @@ public class RegEx {
                 continue
             }
 
-            var subString = [CChar](count: Int(length) + 1, repeatedValue: 0)
+            var subString = [CChar](repeating:0, count: Int(length) + 1)
             for idx in startOffset..<length {
                 subString[Int(idx-startOffset)] = subject[Int(idx)]
             }
-            if let match = String.fromCString(UnsafePointer<CChar>(subString)) {
-                params.append(match)
+            
+            if String(cString: UnsafePointer<CChar>(subString)).characters.count > 0 {
+                params.append(String(cString: UnsafePointer<CChar>(subString)))
             }
         }
 
@@ -146,13 +152,13 @@ public class RegEx {
                 continue
             }
 
-            var subString = [CChar](count: Int(length) + 1, repeatedValue: 0)
+            var subString = [CChar](repeating: 0, count: Int(length) + 1)
             for idx in startOffset..<length {
                 subString[Int(idx-startOffset)] = subject[Int(idx)]
             }
-
-            if let match = String.fromCString(UnsafePointer<CChar>(subString)) {
-                named[grp] = match
+            
+            if String(cString: UnsafePointer<CChar>(subString)).characters.count > 0 {
+                named[grp] = String(cString: UnsafePointer<CChar>(subString))
             }
         }
 
